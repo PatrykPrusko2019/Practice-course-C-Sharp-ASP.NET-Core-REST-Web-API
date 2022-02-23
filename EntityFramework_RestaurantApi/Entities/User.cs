d4937d632382b3ada92be756eea93bd9c59721da
Patryk <patrykprusko@gmai.com>
2022-02-23 20:07:13 +0100

    Add tables with roles and users 	adds 3 types of roles -> 1. user, 2. manager, 3. admin

---
+++ EntityFramework_RestaurantApi/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework_RestaurantApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; } // may be a value -> null
+        public string Nationality { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int RoleId { get; set; } // foreign key in the table with roles
+        public virtual Role Role { get; set; }
+    }
+}
--- EntityFramework_RestaurantApi/Migrations/20220223185352_UserAndRoleAdd.Designer.cs
+++ EntityFramework_RestaurantApi/Migrations/20220223185352_UserAndRoleAdd.Designer.cs
@@ -4,14 +4,16 @@ using EntityFramework_RestaurantApi.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFramework_RestaurantApi.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220223185352_UserAndRoleAdd")]
+    partial class UserAndRoleAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
